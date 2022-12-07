e00c98b66c892b1de0e2f0734491d7741591289b
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-12-07 09:36:58 +0100

    Retrieved customer's data

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,46 @@
+﻿using learning_aspnetcore_mvc_users_and_roles_with_identity.DataAccess;
+using learning_aspnetcore_mvc_users_and_roles_with_identity.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace learning_aspnetcore_mvc_users_and_logins.Controllers
+{
+    [Authorize(Roles = "Customer")]
+    public class CustomerController : Controller
+    {
+        private readonly ILogger<CustomerController> _logger;
+        private readonly AppDbContext _dbContext;
+
+        public CustomerController(ILogger<CustomerController> logger, AppDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Orders()
+        {
+            var userName = User.FindFirstValue(ClaimTypes.Name);
+            var roleName = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value ?? throw new Exception("Role not found");
+            var customer = _dbContext.Users.FirstOrDefault(u => u.UserName == userName) ?? throw new Exception("User not found");
+
+            var orders = _dbContext.Orders
+                .Where(o => o.UserId == customer.Id)
+                .Include(o => o.User)
+                .ToList()
+                .Select(
+                    o => new OrderVM
+                    {
+                        ProductName = o.Product,
+                        Quantity = o.Quantity,
+                        Price = o.Price,
+                        Total = o.Quantity * o.Price,
+                        CustomerName = userName,
+                        RoleName = roleName
+                    });
+
+            return View(orders);
+        }
+    }
+}
---
+++ Models/OrderVM.cs
@@ -0,0 +1,11 @@
+﻿namespace learning_aspnetcore_mvc_users_and_roles_with_identity.Models;
+
+public class OrderVM
+{
+    public string ProductName { get; set; } = default!;
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+    public decimal Total { get; set; }
+    public string CustomerName { get; set; } = default!;
+    public string RoleName { get; set; } = default!;
+}
