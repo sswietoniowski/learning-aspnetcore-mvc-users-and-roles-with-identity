a7c005c39aed025aed9cad71fc4466b5a9720fe8
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-12-07 09:47:37 +0100

    Added some basic view for orders

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,46 +1,47 @@
-﻿using learning_aspnetcore_mvc_users_and_roles_with_identity.DataAccess;
+﻿using System.Security.Claims;
+using learning_aspnetcore_mvc_users_and_roles_with_identity.DataAccess;
 using learning_aspnetcore_mvc_users_and_roles_with_identity.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
 
-namespace learning_aspnetcore_mvc_users_and_logins.Controllers
+namespace learning_aspnetcore_mvc_users_and_roles_with_identity.Controllers;
+
+[Authorize(Roles = "Customer")]
+public class CustomerController : Controller
 {
-    [Authorize(Roles = "Customer")]
-    public class CustomerController : Controller
+    private readonly ILogger<CustomerController> _logger;
+    private readonly AppDbContext _dbContext;
+
+    public CustomerController(ILogger<CustomerController> logger, AppDbContext dbContext)
     {
-        private readonly ILogger<CustomerController> _logger;
-        private readonly AppDbContext _dbContext;
+        _logger = logger;
+        _dbContext = dbContext;
+    }
 
-        public CustomerController(ILogger<CustomerController> logger, AppDbContext dbContext)
-        {
-            _logger = logger;
-            _dbContext = dbContext;
-        }
+    public IActionResult Orders()
+    {
+        var userName = User.FindFirstValue(ClaimTypes.Name);
+        var customer = _dbContext.Users.FirstOrDefault(u => u.UserName == userName) 
+                       ?? throw new Exception("User not found");
+        var roleName = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role && c.Value == "Customer")?.Value 
+                       ?? throw new Exception("Role not found");
 
-        public IActionResult Orders()
-        {
-            var userName = User.FindFirstValue(ClaimTypes.Name);
-            var customer = _dbContext.Users.FirstOrDefault(u => u.UserName == userName) ?? throw new Exception("User not found");
-            var roleName = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role && c.Value == "Customer")?.Value ?? throw new Exception("Role not found");
+        var orders = _dbContext.Orders
+            .Where(o => o.UserId == customer.Id)
+            .Include(o => o.User)
+            .ToList()
+            .Select(
+                o => new OrderVM
+                {
+                    ProductName = o.Product,
+                    Quantity = o.Quantity,
+                    Price = o.Price,
+                    Total = o.Quantity * o.Price,
+                    CustomerName = userName,
+                    RoleName = roleName
+                });
 
-            var orders = _dbContext.Orders
-                .Where(o => o.UserId == customer.Id)
-                .Include(o => o.User)
-                .ToList()
-                .Select(
-                    o => new OrderVM
-                    {
-                        ProductName = o.Product,
-                        Quantity = o.Quantity,
-                        Price = o.Price,
-                        Total = o.Quantity * o.Price,
-                        CustomerName = userName,
-                        RoleName = roleName
-                    });
-
-            return View(orders);
-        }
+        return View(orders);
     }
 }
